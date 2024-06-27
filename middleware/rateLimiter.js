332e9452938e21722b304cc0c293be97ff665158
Nikhil Gupta <nikhilg7404@gmail.com>
2024-06-27 17:08:10 +0530

    Add Middlewares

---
+++ middleware/rateLimiter.js
@@ -0,0 +1,9 @@
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.'
+});
+
+module.exports = { limiter };
