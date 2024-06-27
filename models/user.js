5d6c5a8ca38791dcad3efdd1752f8245432c5224
Nikhil Gupta <nikhilg7404@gmail.com>
2024-06-27 17:36:38 +0530

    Create Schemas

--- models/user.js
+++ models/user.js
@@ -19,19 +19,22 @@ const userSchema = mongoose.Schema({
     timestamps: true,
 });
 
+// Method to compare entered password with stored hashed password
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Middleware to hash password before saving
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
-        next();
+        next(); // If password is not modified, continue
     }
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    const salt = await bcrypt.genSalt(10); // Generate salt for hashing
+    this.password = await bcrypt.hash(this.password, salt); // Hash the password
 });
 
+// Create the User model
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = User; // Export the User model for use in other files
